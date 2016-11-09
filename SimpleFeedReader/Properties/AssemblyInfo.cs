ee1a7e3e6aeb9bae8dca7b335926ce891798a282
RobThree <rob@devcorner.nl>
2016-11-09 23:43:54 +0100

    * Better appveyor integration

--- SimpleFeedReader/Properties/AssemblyInfo.cs
+++ SimpleFeedReader/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Devcorner.nl")]
 [assembly: AssemblyProduct("SimpleFeedReader")]
-[assembly: AssemblyCopyright("Copyright © Devcorner.nl 2014")]
+[assembly: AssemblyCopyright("Copyright © Devcorner.nl 2014 - 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.4.0")]
-[assembly: AssemblyFileVersion("1.0.4.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
